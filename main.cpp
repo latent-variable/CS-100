5a41190c0d885a00026b868005d4fab5eeb97f93
Lino Gonzalez Valdovinos <lgonz041@hammer.cs.ucr.edu>
2017-01-09 16:40:40 -0800

    added to user input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
+
 
 int main()
 {
-
+	std::string name;	
 	std::cout<<"Hello git"<<std::endl;
+	std::cin>>name;
+	std::cout<<"Hello "<< name << "!" <<std::endl;
 
 	return 1;
 }
